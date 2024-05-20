944d98434c599e2ff0c6714edb8958305533463d
Tuan Nguyen <ngoctuanit07@gmail.com>
2024-05-20 16:31:04 +0700

    update

--- taitheme/taiwebsite.py
+++ taitheme/taiwebsite.py
@@ -97,5 +97,5 @@ def download_website_content(url):
 
 
 if __name__ == '__main__':
-    website_url = 'https://www.wrraptheme.com/templates/luno/crm/index.html'
+    website_url = 'https://pixydrops.com/laboix-html/index-3-one-page.html'
     download_website_content(website_url)
